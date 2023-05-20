3641d0700cc1758786dbecd8695bce28a764f548
SvetaLantsman <svetlana@curalife.com>
2023-05-20 11:32:20 +0300

    .

--- src/components/PositiveCounter.js
+++ src/components/PositiveCounter.js
@@ -4,7 +4,7 @@ import Card from "./Card";
 import useCounter from "../hooks/use-counter";
 
 const PositiveCounter = () => {
-  const counter = useCounter();
+  const counter = useCounter(true);
   // const [counter, setCounter] = useState(0);
 
   // useEffect(() => {
--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react';
 
-const useCounter  = () => {
+const useCounter  = (isPositive=true) => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
       const interval = setInterval(() => {
        
-          setCounter((prevCounter) => prevCounter + 1);
+          if(isPositive){
+            setCounter((prevCounter) => prevCounter + 1);
+             }
+          else {
+             setCounter((prevCounter) => prevCounter - 1);
+             }
        
       }, 1000);
 
       return () => clearInterval(interval);
-    }, []);
+    }, [isPositive]);
   
     return counter;
 };
